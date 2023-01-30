4cdcb9d69d55198940afb43b1a7b9c857312ef8d
codermilo <milocodes.io@gmail.com>
2023-01-30 22:34:13 +0000

    feat: added different version

---
+++ precache-manifest.99cc22f8fc83f98499d200599aef3f84.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8780f59fba95bfb921f",
+    "url": "/static/css/main.8bf43e7b.chunk.css"
+  },
+  {
+    "revision": "d8780f59fba95bfb921f",
+    "url": "/static/js/main.d8780f59.chunk.js"
+  },
+  {
+    "revision": "2f691b2d7670db7000b9",
+    "url": "/static/js/1.2f691b2d.chunk.js"
+  },
+  {
+    "revision": "4b3c915728cd039d1221",
+    "url": "/static/js/2.4b3c9157.chunk.js"
+  },
+  {
+    "revision": "90a4c2bb36d3ceb3fc0b",
+    "url": "/static/js/runtime~main.90a4c2bb.js"
+  },
+  {
+    "revision": "e7cbf5163f9037ceca6b67c485c55c63",
+    "url": "/static/media/C9.e7cbf516.png"
+  },
+  {
+    "revision": "f4295c02cbdea5b6542d5ac543d129b7",
+    "url": "/static/media/CLG.f4295c02.png"
+  },
+  {
+    "revision": "ce9ec027cad9001e4b793647d223d2cd",
+    "url": "/static/media/TSM.ce9ec027.png"
+  },
+  {
+    "revision": "4b19283b09d616856a2229341472a976",
+    "url": "/static/media/GG.4b19283b.png"
+  },
+  {
+    "revision": "6148ab9841986c79b874e1017dc6863b",
+    "url": "/static/media/TL.6148ab98.png"
+  },
+  {
+    "revision": "e5ee79102f45ad165c397e9744e30aaf",
+    "url": "/static/media/FLY.e5ee7910.png"
+  },
+  {
+    "revision": "6e314a024bf223ff97208745c7161c48",
+    "url": "/static/media/DIG.6e314a02.png"
+  },
+  {
+    "revision": "15b9c0341de1858952d1add3eafa39a5",
+    "url": "/static/media/IMT.15b9c034.png"
+  },
+  {
+    "revision": "5edf4e0d5037fedf2bd41138f52c0635",
+    "url": "/static/media/100T.5edf4e0d.png"
+  },
+  {
+    "revision": "19b52d43c17f12384b7c44a0e5d3803d",
+    "url": "/static/media/C9.19b52d43.png"
+  },
+  {
+    "revision": "572038b895c328989e77ce02aa9d3cf8",
+    "url": "/static/media/Top_icon.572038b8.webp"
+  },
+  {
+    "revision": "adc95f58cd770f5beaaedefddc2436d6",
+    "url": "/static/media/Middle_icon.adc95f58.webp"
+  },
+  {
+    "revision": "0e8fb1588a69fe63129073c32100a71e",
+    "url": "/static/media/Jungle_icon.0e8fb158.webp"
+  },
+  {
+    "revision": "7953c83a714f9f99ab0189568c64d9cc",
+    "url": "/static/media/Bottom_icon.7953c83a.webp"
+  },
+  {
+    "revision": "8b6586ec3d3ebd4429cfc7e2f68cee2a",
+    "url": "/static/media/Support_icon.8b6586ec.webp"
+  },
+  {
+    "revision": "a7b19b2961226fa0a69732d11bf1c082",
+    "url": "/static/media/Berserker.a7b19b29.png"
+  },
+  {
+    "revision": "531e1275f28f774fccc9e1b251a4e8a5",
+    "url": "/static/media/Danny.531e1275.png"
+  },
+  {
+    "revision": "6130ea12b7b1f6e7c9c1b397210d2248",
+    "url": "/static/media/FBI.6130ea12.png"
+  },
+  {
+    "revision": "87fb9b7b9d89e0d58efde151b6d74fa7",
+    "url": "/static/media/HansSama.87fb9b7b.png"
+  },
+  {
+    "revision": "950e91f0043bdd7eccd2a7f6994834d7",
+    "url": "/static/media/Johnsun.950e91f0.png"
+  },
+  {
+    "revision": "1d6e13cd44884530cec0ccabead80951",
+    "url": "/static/media/Luger.1d6e13cd.png"
+  },
+  {
+    "revision": "54862937e2d5ee8f50f8f6869fad2b57",
+    "url": "/static/media/Tactical.54862937.png"
+  },
+  {
+    "revision": "d6f9fcc44ef2424428843549e60c8e60",
+    "url": "/static/media/Blaber.d6f9fcc4.png"
+  },
+  {
+    "revision": "a15b9b17b6bc1d6940e378bfc0d4ea80",
+    "url": "/static/media/Closer.a15b9b17.png"
+  },
+  {
+    "revision": "8db1a1446e2868775ecbbebf224bf822",
+    "url": "/static/media/Contractz.8db1a144.png"
+  },
+  {
+    "revision": "314c6e9e4635afe36a91b67a9c8d4eff",
+    "url": "/static/media/Josedeodo.314c6e9e.png"
+  },
+  {
+    "revision": "63a984d960c4754ec31ea92be6c99fe1",
+    "url": "/static/media/River.63a984d9.png"
+  },
+  {
+    "revision": "e1fd17b2bff6de3039d3cf5550c8b8cb",
+    "url": "/static/media/Santorin.e1fd17b2.png"
+  },
+  {
+    "revision": "663972f848cec3055e463fc8ddb65c31",
+    "url": "/static/media/Spica.663972f8.png"
+  },
+  {
+    "revision": "e1a1703576cc472a48cd088e7fc67889",
+    "url": "/static/media/eXyu.e1a17035.png"
+  },
+  {
+    "revision": "4938a0e07a2c4a661581af4937302b61",
+    "url": "/static/media/AblazeOlive.4938a0e0.png"
+  },
+  {
+    "revision": "b6200c0c715e2b8d29bd87504d67507d",
+    "url": "/static/media/Bjergsen.b6200c0c.png"
+  },
+  {
+    "revision": "61167415b768b525e7f9e332593a896b",
+    "url": "/static/media/Blue.61167415.png"
+  },
+  {
+    "revision": "6936a408e921332820e9d5dabdafeed6",
+    "url": "/static/media/Jojopyun.6936a408.png"
+  },
+  {
+    "revision": "69444759411e3bfcfd54ab7dee60f9d1",
+    "url": "/static/media/Maple.69444759.png"
+  },
+  {
+    "revision": "9c98eafe7df45e1f37e9f0b3ae73f204",
+    "url": "/static/media/Palafox.9c98eafe.png"
+  },
+  {
+    "revision": "812b4208239e9fbc0ca70c35130aecfb",
+    "url": "/static/media/Aphromoo.812b4208.png"
+  },
+  {
+    "revision": "69c91546a558b0c845763b33a2aa0950",
+    "url": "/static/media/Biofrost.69c91546.png"
+  },
+  {
+    "revision": "2cfdc711e651307c20cd3f998447a96b",
+    "url": "/static/media/Chime.2cfdc711.png"
+  },
+  {
+    "revision": "b25fe807698d3187ec9da5fb2b418786",
+    "url": "/static/media/Olleh.b25fe807.png"
+  },
+  {
+    "revision": "cc1b1089b2190c0426e2b953e57b0f44",
+    "url": "/static/media/Zven.cc1b1089.png"
+  },
+  {
+    "revision": "d8e26f67e94e5f707703ed2c8f2ee29e",
+    "url": "/static/media/Bwipo.d8e26f67.png"
+  },
+  {
+    "revision": "6c882015336acccde825a33c3be9befb",
+    "url": "/static/media/Dhokla.6c882015.png"
+  },
+  {
+    "revision": "2c8e4b18f621ed19c3aa7f2adde4226b",
+    "url": "/static/media/FakeGod.2c8e4b18.png"
+  },
+  {
+    "revision": "42f5b719b2f8c662ea5c39af14018154",
+    "url": "/static/media/Impact.42f5b719.png"
+  },
+  {
+    "revision": "e6a432c983733c4ce6f659579d7df83e",
+    "url": "/static/media/Licorice.e6a432c9.png"
+  },
+  {
+    "revision": "59b87257a6a0e17aa3c681d280a953b5",
+    "url": "/static/media/Solo.59b87257.png"
+  },
+  {
+    "revision": "976d10d1f54f03f4881040882dcf4418",
+    "url": "/static/media/Cross.976d10d1.svg"
+  },
+  {
+    "revision": "7fd847fdc0c6d25f9d546cb626113f5a",
+    "url": "/static/media/SoloKill.7fd847fd.svg"
+  },
+  {
+    "revision": "f6410eee79afad23a76d7336950d8d3b",
+    "url": "/static/media/Deaths.f6410eee.svg"
+  },
+  {
+    "revision": "f162e156a8725a56ca72f6cf94b57e12",
+    "url": "/static/media/Assist.f162e156.svg"
+  },
+  {
+    "revision": "d19b3e7c0ccdf01d2bec1bdee17a4b69",
+    "url": "/static/media/CS.d19b3e7c.svg"
+  },
+  {
+    "revision": "03595e96284b9bb934b06fbf31a0e7ef",
+    "url": "/static/media/FirstBlood.03595e96.svg"
+  },
+  {
+    "revision": "6972130c72fedb2734dfa3c1d469a781",
+    "url": "/static/media/FirstTower.6972130c.svg"
+  },
+  {
+    "revision": "425e656d15b8256a3e902ff5f8909f1b",
+    "url": "/static/media/Dragon.425e656d.svg"
+  },
+  {
+    "revision": "6cf693eac7a75d22d75e072c2793f817",
+    "url": "/static/media/FirstBaron.6cf693ea.svg"
+  },
+  {
+    "revision": "8eb2985d4f1923b9598e7a6d66e76798",
+    "url": "/static/media/Tick.8eb2985d.svg"
+  },
+  {
+    "revision": "78b004840f2b22702eea4f49eb4db946",
+    "url": "/index.html"
+  }
+];
