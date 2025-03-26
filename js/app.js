0e6267682c47276ed4322be5e185d37ffde5df6c
Dan <milocodes.io@gmail.com>
2025-03-26 15:14:03 +0000

    updated colours of backdrop letters in light mode and reduced blur

--- js/app.js
+++ js/app.js
@@ -29,6 +29,47 @@ $('header').on('click', '.hamRotate', () => {
 })
 
 /*--- Banner text animation using GSAP and Splitting.js ---*/
+// Splitting Banner text into chars
+Splitting();
+
+let DOM = {
+    content: {
+        home: {
+            section: document.querySelector('.content__item--home'),
+            get chars() {
+                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace');
+            },
+            isVisible: true
+        }
+    }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    gsap.set(DOM.content.home.chars, {
+        y: '-100%',
+        opacity: 0
+    });
+
+    const timelineSettings = {
+        staggerValue: 0.014,
+        charsDuration: 0.5
+    };
+
+    let tl = gsap.timeline({ paused: true })
+        .staggerTo(DOM.content.home.chars, timelineSettings.charsDuration, {
+            ease: 'Power3.easeIn',
+            y: '0%',
+            opacity: 1
+        }, timelineSettings.staggerValue);
+
+    tl.play();
+
+    // Reverse the timeline after 2 seconds
+    // setTimeout(() => {
+    //     tl.reverse();
+    // }, 2000);
+});
+
 /*--- Page transition using GSAP ---*/
 /*--- Form validation using vanilla JS ---*/
 /*--- Theme picker (saving to local storage) ---*/
