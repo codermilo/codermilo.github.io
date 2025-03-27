7913d2dc1caea57a9c4d3f6dc8e3b6c30516d97e
Dan <milocodes.io@gmail.com>
2025-03-27 14:00:58 +0000

    Updating transition animation.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 /*--- Slide out functionality ---*/
 // !!! To fix:
+
 // - the menu jumping to 100vw at larger screen sizes during transition 
 const $menu = $('.sidebar');
 const $hamburger = $('.hamRotate');
@@ -32,42 +33,99 @@ $('header').on('click', '.hamRotate', () => {
 // Splitting Banner text into chars
 Splitting();
 
+// Grabbing content that should be animated
 let DOM = {
     content: {
         home: {
             section: document.querySelector('.content__item--home'),
             get chars() {
                 return this.section.querySelectorAll('.anim__text .word > .char, .whitespace');
-            },
-            isVisible: true
+            }
+        }, sidebar: {
+            section: document.querySelector('.sidebar'),
+            get links() {
+                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace, .social-links a');
+            }
+        }, nav: {
+            section: document.querySelector('header'),
+            get links() {
+                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace, h1, .theme-toggle__container div');
+            }
+        }, backdrop: {
+            section: document.querySelector('.backdrop'),
+            get chars() {
+                return this.section.querySelectorAll('span');
+            }
         }
     }
 };
 
+// On page load set up timeline and animate
 window.addEventListener('DOMContentLoaded', () => {
     gsap.set(DOM.content.home.chars, {
         y: '-100%',
         opacity: 0
     });
+    gsap.set(DOM.content.sidebar.links, {
+        y: '-100%',
+        opacity: 0
+    });
+    gsap.set(DOM.content.nav.links, {
+        y: '-100%',
+        opacity: 0
+    });
+    gsap.set(DOM.content.backdrop.chars[0], {
+        y: '-100%',
+        x: '-100%'
+    });
+    gsap.set(DOM.content.backdrop.chars[1], {
+        y: '-100%',
+        x: '100%'
+    });
+    gsap.set(DOM.content.backdrop.chars[2], {
+        y: '100%',
+        x: '-100%'
+    });
+    gsap.set(DOM.content.backdrop.chars[3], {
+        y: '100%',
+        x: '100%'
+    });
 
     const timelineSettings = {
-        staggerValue: 0.014,
-        charsDuration: 0.5
+        staggerValue: 0.009,
+        // staggerValue: 0.011,
+        // staggerValue: 0.014,
+        charsDuration: 2
     };
 
     let tl = gsap.timeline({ paused: true })
+        .to(DOM.content.backdrop.chars, {
+            ease: 'elastic.out(1,0.9)',
+            y: '0%',
+            x: 0,
+            duration: 1.25
+        })
+        .staggerTo(DOM.content.nav.links, timelineSettings.charsDuration, {
+            ease: 'Expo.easeOut',
+            y: '0%',
+            opacity: 1
+        }, timelineSettings.staggerValue)
+        .staggerTo(DOM.content.sidebar.links, timelineSettings.charsDuration, {
+            ease: 'Expo.easeOut',
+            y: '0%',
+            opacity: 1
+        }, timelineSettings.staggerValue)
         .staggerTo(DOM.content.home.chars, timelineSettings.charsDuration, {
-            ease: 'Power3.easeIn',
+            ease: 'Expo.easeOut',
             y: '0%',
             opacity: 1
         }, timelineSettings.staggerValue);
 
     tl.play();
-
-    // Reverse the timeline after 2 seconds
+    //Reverse the timeline after 2 seconds
     // setTimeout(() => {
     //     tl.reverse();
-    // }, 2000);
+    // }, 15000);
 });
 
 /*--- Page transition using GSAP ---*/
