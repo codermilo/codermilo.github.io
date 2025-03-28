9aa7cd7c9f15920031eef7cee227e50d9cba906c
Dan <milocodes.io@gmail.com>
2025-03-28 17:34:36 +0000

    stashing

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 
 /*--- Slide out functionality ---*/
 // !!! To fix:
-
 // - the menu jumping to 100vw at larger screen sizes during transition 
+
 const $menu = $('.sidebar');
 const $hamburger = $('.hamRotate');
 
@@ -37,19 +37,9 @@ Splitting();
 let DOM = {
     content: {
         home: {
-            section: document.querySelector('.content__item--home'),
+            section: document.querySelector('body'),
             get chars() {
-                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace');
-            }
-        }, sidebar: {
-            section: document.querySelector('.sidebar'),
-            get links() {
-                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace, .social-links a');
-            }
-        }, nav: {
-            section: document.querySelector('header'),
-            get links() {
-                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace, h1, .theme-toggle__container div');
+                return this.section.querySelectorAll('.anim__text .word > .char, .whitespace, .social-links a, .logo__container h1, .theme-toggle__container div');
             }
         }, backdrop: {
             section: document.querySelector('.backdrop'),
@@ -60,20 +50,21 @@ let DOM = {
     }
 };
 
-// On page load set up timeline and animate
-window.addEventListener('DOMContentLoaded', () => {
-    gsap.set(DOM.content.home.chars, {
+// Settings that every element will get
+const timelineSettings = {
+    staggerValue: 0.004,
+    charsDuration: 1.25
+};
+
+// On function call set up timeline and animate
+function pageEnterAnimation() {
+    // Set every element to animated to -100% Y so it is out of view before the animation starts
+    gsap.set(".anim__text .word > .char, .whitespace, .social-links a, .logo__container h1, .theme-toggle__container div", {
         y: '-100%',
         opacity: 0
     });
-    gsap.set(DOM.content.sidebar.links, {
-        y: '-100%',
-        opacity: 0
-    });
-    gsap.set(DOM.content.nav.links, {
-        y: '-100%',
-        opacity: 0
-    });
+
+    // Individually animating the backdrop chars as they have different starting points
     gsap.set(DOM.content.backdrop.chars[0], {
         y: '-100%',
         x: '-100%'
@@ -91,44 +82,44 @@ window.addEventListener('DOMContentLoaded', () => {
         x: '100%'
     });
 
-    const timelineSettings = {
-        staggerValue: 0.009,
-        // staggerValue: 0.011,
-        // staggerValue: 0.014,
-        charsDuration: 2
-    };
-
+    // Create the timeline for the animations
     let tl = gsap.timeline({ paused: true })
-        .to(DOM.content.backdrop.chars, {
+        .to(".backdrop span", {
             ease: 'elastic.out(1,0.9)',
             y: '0%',
             x: 0,
-            duration: 1.25
+            duration: 2.25
         })
-        .staggerTo(DOM.content.nav.links, timelineSettings.charsDuration, {
-            ease: 'Expo.easeOut',
-            y: '0%',
-            opacity: 1
-        }, timelineSettings.staggerValue)
-        .staggerTo(DOM.content.sidebar.links, timelineSettings.charsDuration, {
-            ease: 'Expo.easeOut',
-            y: '0%',
-            opacity: 1
-        }, timelineSettings.staggerValue)
-        .staggerTo(DOM.content.home.chars, timelineSettings.charsDuration, {
+
+        .staggerTo(".anim__text .word > .char, .whitespace, .social-links a, .logo__container h1, .theme-toggle__container div", timelineSettings.charsDuration, {
             ease: 'Expo.easeOut',
             y: '0%',
             opacity: 1
         }, timelineSettings.staggerValue);
 
+    // Play the animation
     tl.play();
-    //Reverse the timeline after 2 seconds
-    // setTimeout(() => {
-    //     tl.reverse();
-    // }, 15000);
-});
 
-/*--- Page transition using GSAP ---*/
+};
+
+// Exit function 
+function pageLeaveAnimation() {
+    return gsap.timeline()
+        .to(".backdrop span", {
+            ease: 'elastic.out(1,0.9)',
+            y: '-100%',
+            x: 0,
+            duration: 2.25
+        })
+        .staggerTo(".anim__text .word > .char, .whitespace, .social-links a, .logo__container h1, .theme-toggle__container div", timelineSettings.charsDuration, {
+            ease: 'Expo.easeIn',
+            y: '-100%',
+            opacity: 0
+        }, 0.004);
+}
+
+/*--- Page transition using Barba JS ---*/
+
 /*--- Form validation using vanilla JS ---*/
 /*--- Theme picker (saving to local storage) ---*/
 /*--- Cursor functionality using GSAP ---*/
